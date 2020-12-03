ae5fa3ec120b72b914be55110267bc95d10ab9ba
pineapple34 <pineapple6934@gmail.com>
2020-12-03 14:19:12 +0500

    Initial commit

---
+++ Boxes1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Boxes1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lb1.SelectedIndex == 5 || lb1.SelectedIndex == 6) MessageBox.Show(lb1.SelectedValue.ToString().Substring(37) + ", выходной");
+            else MessageBox.Show(lb1.SelectedValue.ToString().Substring(37));
+        }
+    }
+}
